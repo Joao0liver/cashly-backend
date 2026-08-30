57359ffc7a8cd6f03007f4851143c5f1ae48064a
Joao0liver <joaoolv.profissional@gmail.com>
2026-08-30 10:50:44 -0300

    feat: Criação do view Vendas + página HTML

--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Vendas
 
-# Create your views here.
+def vendas(request):
+    vendas = Vendas.objects.all()
+    return render(request, 'vendas.html', {'vendas': vendas})
