06264423618e50c54cbeaa345d61dd4d9480191f
Joao0liver <joaoolv.profissional@gmail.com>
2026-08-30 10:46:47 -0300

    feat: Criação do model Vendas

--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Vendas(models.Model):
+
+    data = models.DateField()
+
+    TIPO_PAGAMENTO = [
+        ('DINHEIRO', 'Dinheiro'),
+        ('PIX', 'Pix'),
+        ('DEBITO', 'Débito'),
+        ('CREDITO', 'Crédito'),
+    ]
+
+    tipo_pagamento = models.CharField(max_length=10, choices=TIPO_PAGAMENTO)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    BANDEIRA_CARTAO = [
+        ('VISA', 'Visa'),
+        ('MASTERCARD', 'Mastercard'),
+        ('ELO', 'Elo'),
+    ]
+
+    bandeira_cartao = models.CharField(max_length=30, choices=BANDEIRA_CARTAO, blank=True)
+    responsavel = models.CharField(max_length=100)
+    num_caixa =  models.CharField(max_length=20)
+    obs = models.TextField(blank=True)
+    conferido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.valor_total
