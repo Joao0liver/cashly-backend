617f0351e967d685393fcdab26e7454637daffc9
Joao0liver <joaoolv.profissional@gmail.com>
2026-09-02 15:30:05 -0300

    docs: Comentários nas urls de relatórios

--- relatorios/urls.py
+++ relatorios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# Caminho para a tela de seleção de tipo de relatório
 urlpatterns = [
     path('', views.relatorios, name='relatorios'),
     path('pdf/', views.relatorio_vendas_pdf, name='relatorio_vendas_pdf'),
